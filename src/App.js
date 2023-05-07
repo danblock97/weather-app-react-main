5e47360c79c9b2a3a3b1e05a312e7e3e0015cabd
danblock97 <generalgoldiez@gmail.com>
2023-05-07 13:59:57 +0100

    Weather.io

---
+++ src/App.js
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+function WeatherApp() {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
+  const [showResults, setShowResults] = useState(true);
+
+  const API_KEY = process.env.REACT_APP_API_KEY;
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${API_KEY}`;
+
+  const searchLocation = (event) => {
+    if (event.key === "Enter") {
+      axios
+        .get(url)
+        .then((response) => {
+          setData(response.data);
+          console.log(response.data);
+          toast.success("Weather data fetched successfully");
+          setShowResults(true);
+        })
+        .catch((error) => {
+          toast.error("Error fetching weather data");
+        });
+      setLocation("");
+    }
+  };
+
+  const clearResults = () => {
+    setData({});
+    setShowResults(false);
+  };
+
+  return (
+    <div className="app">
+      <ToastContainer />
+      <div className="search">
+        <input
+          value={location}
+          onChange={(event) => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+          placeholder="Enter Location"
+          type="text"
+        />
+      </div>
+      {showResults && (
+        <div className="container">
+          <div className="top">
+            <div className="location">
+              <p>{data.name}</p>
+            </div>
+            <div className="temp">
+              {data.main ? <h1>{data.main.temp.toFixed()}°C</h1> : null}
+            </div>
+            <div className="description">
+              {data.weather ? <p>{data.weather[0].main}</p> : null}
+            </div>
+          </div>
+
+          {data.name !== undefined && (
+            <div className="bottom">
+              <div className="feels">
+                {data.main ? (
+                  <p className="bold">{data.main.feels_like.toFixed()}°C</p>
+                ) : null}
+                <p>Feels Like</p>
+              </div>
+              <div className="humidity">
+                {data.main ? (
+                  <p className="bold">{data.main.humidity}%</p>
+                ) : null}
+                <p>Humidity</p>
+              </div>
+              <div className="wind">
+                {data.wind ? (
+                  <p className="bold">{data.wind.speed.toFixed()} MPH</p>
+                ) : null}
+                <p>Wind Speed</p>
+              </div>
+              <button onClick={clearResults}>Clear Results</button>
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default WeatherApp;
